4f6ffa14ffb90c3c58145d7724baf0f976b7a4b2
jhanlee25 <jhanlee25@gmail.com>
2017-02-07 13:43:47 -0500

    Updated sqoot.py to v3.0

--- api/sqoot.py
+++ api/sqoot.py
@@ -3,10 +3,12 @@ import json
 import config
 
 def searchBy(location, radius):
-    api_key = config.sqoot('public_key')
+    api_key = config.sqoot('apiKey_public')
     url = 'http://api.sqoot.com/v2/deals/?location=' + location + '&radius=' + radius + '&api_key=' + api_key
     
-    json.load(urllib2.urlopen(url))
+    json_obj = json.load(urllib2.urlopen(url))
+    
+    data = json.load(json_obj)
     
     for deals in data['deals']:
         print (deals['deal']['title'])
